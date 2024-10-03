0038abf52910ea0033c7acaa77d7945daf38ac7e
mrodriguezul <miguel.rodriguez.u@gmail.com>
2024-10-03 09:27:37 -0500

    view status

--- src/main/java/dev/mrodriguezul/Main.java
+++ src/main/java/dev/mrodriguezul/Main.java
@@ -4,5 +4,18 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to the fake ATM!");
 
+        String accountNumber = "123456789";
+        double balance = 1000.0;
+        int pin = 1234;
+
+        int[] transactionAmounts = {100, -200, 300, -400, 500};
+
+        balance+=transactionAmounts[0];
+        if(balance > 0 && pin == 1234){
+            System.out.println("Welcome, Your balance is: " + balance);
+        }
+
+        String status = (balance < 0) ? "overdrawn" : "in credit";
+        System.out.println("Your account is " + status);
     }
 }
